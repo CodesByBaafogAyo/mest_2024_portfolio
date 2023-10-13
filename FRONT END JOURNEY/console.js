399447b07d114164d9556b7d5b6237c3f43087ad
CodesByBaafogAyo <ayomideracheal12@gmail.com>
2023-10-13 09:57:13 +0000

    I reorrganized my whole folder and took some files out, files that were mistakenly pushed

--- FRONT END JOURNEY/console.js
+++ FRONT END JOURNEY/console.js
@@ -191,3 +191,17 @@ switch (day) {
         console.log("There is no such day") 
             break 
 }
+
+
+for (var i = 1; i <= 10; i++) {
+    if(i == 1) {
+        console.log("Gold medal")
+    } else if (i == 2) {
+        console.log("Silver medal")
+    } else if (i == 3) {
+        console.log("Bronze medal")
+    } else {
+        //this block will run if no condition matches
+        console.log(i)
+    }
+}
